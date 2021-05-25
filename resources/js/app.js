985eb0d06d9a644546cdb12f89cdf8df797a26f1
Masa H <masamasa0669@gmail.com>
2021-05-25 22:34:31 +0900

    create header component

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,8 @@
  */
 
 require('./bootstrap');
+import Vue from "vue";
+import HeaderComponent from "./components/HeaderComponent";
 
 window.Vue = require('vue').default;
 
@@ -20,6 +22,7 @@ window.Vue = require('vue').default;
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('header-component', HeaderComponent);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container-fluid bg-dark mb3">
+    <div class="container">
+      <nav class="navber navbar-dark">
+        <span class="navber-brand mb-0 h1">Study App</span>
+        <div>
+          <button class="btn btn-success">List</button>
+          <button class="btn btn-success">Add</button>
+        </div>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
