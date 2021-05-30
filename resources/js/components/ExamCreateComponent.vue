04e10f6fcd43a1108e31de257a19e0f6bda2fffd
Masa H <masamasa0669@gmail.com>
2021-05-30 13:10:15 +0900

    edit ExamCreateComponent layout

--- resources/js/components/ExamCreateComponent.vue
+++ resources/js/components/ExamCreateComponent.vue
@@ -2,10 +2,30 @@
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-sm-6">
+        <h4 class="title border-bottom">過去問作成</h4>
         <form v-on:submit.prevent="submit">
-          <div class="form-group row">
-            <label for="ExamDate" class="col-sm-3 col-form-label">試験日</label>
-            <input type="text" class="col-sm-9 form-control" id="ExamDate" v-model="exam.exam_date">
+          <div class="form-group">
+            <label for="ExamDate" class="col-form-label">試験日</label>
+            <input type="text" class= "form-control" id="ExamDate" v-model="exam.exam_date">
+          </div>
+          <div class="form-group">
+          <p class="col-form-label" for="Subject">科目</p>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="checkbox" id="law" value="law" >
+            <label class="form-check-label" for="law">法規</label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="checkbox" id="airplane" value="airplane" >
+            <label class="form-check-label" for="airplane">機体</label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="checkbox" id="engine" value="engine" >
+            <label class="form-check-label" for="engine">発動機</label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="checkbox" id="elec_equipment" value="elec_equipment" >
+            <label class="form-check-label" for="elec_equipment">電気/電子装備品</label>
+          </div>
           </div>
           <button type="submit" class="btn btn-primary">作成</button>
         </form>
@@ -16,6 +36,7 @@
 
 <script>
 export default {
+
   data: function() {
     return {
       exam: {}
