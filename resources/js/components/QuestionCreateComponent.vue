e65da2d1983951fb260ddc5a9f4d64eebcd5460a
Masa H <masamasa0669@gmail.com>
2021-06-14 09:58:56 +0900

    問題文の登録フォーム完成

--- resources/js/components/QuestionCreateComponent.vue
+++ resources/js/components/QuestionCreateComponent.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="container">
-    問題作成
+    <div class="row justify-content-center">
+      <div class="col-sm-6">
+        <h4 class="title border-bottom">問題文登録</h4>
+        <form action="">
+          <div class="form-group" v-for="n in 25" :key="n" >
+            <label for="QuestionId" class="col-form-label">{{"問題 " + n}}</label>
+            <input type="text" class="form-control" id="QuestionText">
+          </div>
+          <button type="submit" class="btn btn-primary">作成</button>
+        </form>
+      </div>
+    </div>
+
   </div>
 </template>
 
