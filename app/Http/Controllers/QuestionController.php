5895aa84b691643a8ef9c57f4faa0aa323ca82f0
Masa H <masamasa0669@gmail.com>
2021-06-17 05:18:51 +0900

    Laravel側でfileの保存(storageに)

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class QuestionController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $file_name = $request->file->getClientOriginalName();
+        $request->file->storeAs('public/', $file_name);
+    }
 }
+
