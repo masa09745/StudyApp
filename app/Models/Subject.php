fad6973f0dbc8f7a34730677b8acc6318745e0f0
Masa H <masamasa0669@gmail.com>
2021-08-03 13:54:50 +0900

    subject table復活。　exam tableと1対多の関係に

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -5,17 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-
-class Exam extends Model
+class Subject extends Model
 {
     protected $fillable = [
-        'date'
+        'name'
     ];
 
-    public function Subjects()
+    public function Exam()
     {
-        return $this->hasMany(Subjects::class);
+        return $this->belongsTo(Exam::class);
     }
-    
     use HasFactory;
 }
