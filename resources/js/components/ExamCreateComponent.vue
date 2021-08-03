6ccbf626d26e588f75ffadb18a7cd168e78e8515
Masa H <masamasa0669@gmail.com>
2021-08-03 13:31:19 +0900

    不要なファイル削除。　過去問登録を試験日のみに(vue)

--- resources/js/components/ExamCreateComponent.vue
+++ resources/js/components/ExamCreateComponent.vue
@@ -2,19 +2,16 @@
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-sm-6">
-        <h4 class="title border-bottom">過去問作成</h4>
+        <form v-on:submit.prevent="submit">
+          <h4 class="title border-bottom">過去問作成</h4>
           <div class="form-group">
             <label for="ExamDate" class="col-form-label">試験日</label>
-            <input type="text" class= "form-control" id="ExamDate" v-model="date">
+            <input type="text" class= "form-control" id="ExamDate" v-model="exam.date">
           </div>
-          <div class="form-group">
-            <div class="custom-file">
-              <input type="file" class="custom-file-input" @change="confirmFile" v-if="view">
-              <label class="custom-file-label" for="uploadfile">過去問を選んでください</label>
-            </div>
-          </div>
-          <p v-if="confirmedFile">{{file.name}}</p>
-          <button @click="uploadExam" class="btn btn-primary">作成</button>
+          <button type="submit" class="btn btn-primary">作成</button>
+
+        </form>
+
       </div>
     </div>
   </div>
@@ -25,31 +22,12 @@ export default {
 
   data: function() {
     return {
-      date: "",
-      file: "",
-      view: true,
-      confirmedFile: ""
-
+      exam: {}
     }
   },
   methods: {
-    confirmFile(e){
-      this.file = e.target.files[0];
-      this.createFile(this.file);
-    },
-    createFile(file) {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload = e => {
-        this.confirmedFile = e.target.result;
-      };
-    },
-    uploadExam() {
-      let data = new FormData();
-      data.append("date", this.date);
-      data.append("file", this.file);
-
-      axios.post('/api/exams', data)
+    submit(){
+      axios.post('/api/exams', this.exam)
         .then(res => {
           this.$router.push({name: 'exam.list'});
         });
