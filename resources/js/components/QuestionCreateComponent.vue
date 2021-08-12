6e6fef930f2d95f3a2188c3224c4d3d81ee31e26
Masa H <masamasa0669@gmail.com>
2021-08-12 22:07:56 +0900

    問題作成画面のレイアウト変更

--- resources/js/components/QuestionCreateComponent.vue
+++ resources/js/components/QuestionCreateComponent.vue
@@ -5,8 +5,38 @@
         <form v-on:submit.prevent="submit">
           <h4 class="title border-bottom">過去問作成</h4>
           <div class="form-group">
-            <label for="ExamDate" class="col-form-label">試験日</label>
-            <input type="text" class= "form-control" id="ExamDate" v-model="exam.date">
+
+            <label for="QuestionSubject" class="col-form-label">科目</label>
+            <div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" id="row" >
+                <label class="form-check-label" for="row">法規</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" id="airplane" >
+                <label class="form-check-label" for="row">機体</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" id="engine" >
+                <label class="form-check-label" for="row">発動機</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" id="electronics" >
+                <label class="form-check-label" for="electronics">電気・電子装備品</label>
+              </div>
+            </div>
+            <label for="QuestionText" class="col-form-label">問題文</label>
+            <input type="text" class= "form-control" id="QuestionText">
+            <label for="QuestionCorrect" class="col-form-label">正解</label>
+            <input type="text" class= "form-control" id="QuestionCorrect">
+            <label for="QuestionIncorrect1" class="col-form-label">不正解1</label>
+            <input type="text" class= "form-control" id="QuestionIncorrect1">
+            <label for="QuestionInorrect2" class="col-form-label">不正解2</label>
+            <input type="text" class= "form-control" id="QuestionIncorrect2">
+            <label for="QuestionIncorrect3" class="col-form-label">不正解3</label>
+            <input type="text" class= "form-control" id="QuestionIncorrect3">
+            <label for="QuestionExplanation" class="col-form-label">解説</label>
+            <textarea class= "form-control" id="QuestionExplanation"/>
           </div>
           <button type="submit" class="btn btn-primary">作成</button>
 
@@ -20,18 +50,5 @@
 <script>
 export default {
 
-  data: function() {
-    return {
-      exam: {}
-    }
-  },
-  methods: {
-    submit(){
-      axios.post('/api/exams', this.exam)
-        .then(res => {
-          this.$router.push({name: 'exam.list'});
-        });
-    }
-  }
 }
 </script>
