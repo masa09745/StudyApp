2fa34ef80522b57e071a48b850c3d9722dddab5a
Masa H <masamasa0669@gmail.com>
2021-08-27 23:26:10 +0900

    選択肢作成の表示完成

--- resources/js/components/ChoiceCreateComponent.vue
+++ resources/js/components/ChoiceCreateComponent.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
+    <label for="QuestionChoice" class="col-form-label">選択肢1</label>
+    <input type="text" class= "form-control" id="QuestionChoice">
+    <label for="QuestionChoice" class="col-form-label">選択肢2</label>
+    <input type="text" class= "form-control" id="QuestionChoice">
+    <label for="QuestionChoice" class="col-form-label">選択肢3</label>
+    <input type="text" class= "form-control" id="QuestionChoice">
+    <label for="QuestionChoice" class="col-form-label">選択肢4</label>
+    <input type="text" class= "form-control" id="QuestionChoice">
   </div>
 </template>
 
