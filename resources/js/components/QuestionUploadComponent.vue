801a3ba8eeafe36a928f384bd16d9484a821434d
Masa H <masamasa0669@gmail.com>
2021-09-14 22:36:43 +0900

    ファイルアップロードフォーム作成

--- resources/js/components/QuestionUploadComponent.vue
+++ resources/js/components/QuestionUploadComponent.vue
@@ -1,6 +1,24 @@
 <template>
-  <div class="container">
-    
+  <div class="container-fluid">
+    <div class="row justify-content-center">
+      <div class="col-md-4">
+        <h4 class="border-bottom">過去問データアップロード</h4>
+        <div class="form-group">
+          <div class="custom-file">
+            <input type="file" class="custom-file-imput" id="uploadFile">
+            <label class="custom-file-label" for="uploadFile">問題データを選んでください</label>
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="custom-file">
+            <input type="file" class="custom-file-imput" id="uploadFile">
+            <label class="custom-file-label" for="uploadFile">選択肢データを選んでください</label>
+          </div>
+        </div>
+        <button type="submit" class="btn btn-primary">アップロード</button>
+      </div>
+    </div>
+
   </div>
 </template>
 
