5b88351caff7ce825c9ab1fb008a3215e4f1ec91
Masa H <masamasa0669@gmail.com>
2021-09-18 00:44:33 +0900

    問題データのアップロードvue変更

--- resources/js/components/QuestionUploadComponent.vue
+++ resources/js/components/QuestionUploadComponent.vue
@@ -3,17 +3,10 @@
     <div class="row justify-content-center">
       <div class="col-md-4">
         <h4 class="border-bottom">過去問データアップロード</h4>
-        <div class="form-group">
-          <div class="custom-file">
-            <input type="file" class="custom-file-imput" id="uploadFile">
-            <label class="custom-file-label" for="uploadFile">問題データを選んでください</label>
-          </div>
-        </div>
-        <div class="form-group">
-          <div class="custom-file">
-            <input type="file" class="custom-file-imput" id="uploadFile">
-            <label class="custom-file-label" for="uploadFile">選択肢データを選んでください</label>
-          </div>
+        <div class="uploadArea">
+          <label>
+            <input type="file" class="uploadFile">ファイルを選択
+          </label>
         </div>
         <button type="submit" class="btn btn-primary">アップロード</button>
       </div>
@@ -29,5 +22,20 @@ export default {
 </script>
 
 <style>
+.uploadArea{
+  margin: 15px 0;
+}
+.uploadFile{
+  display: none;
+}
 
+label {
+  color: black;
+  background-color: white;
+  cursor: pointer;
+  padding: 10px 50px;
+  margin: 0;
+  border: solid 1px #333333;
+  border-radius: 0.3rem;
+}
 </style>
