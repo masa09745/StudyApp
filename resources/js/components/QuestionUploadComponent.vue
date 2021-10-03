689e7bf6ff44f08ece56a406f6835e4a32db7fd7
Masa H <masamasa0669@gmail.com>
2021-10-03 13:03:05 +0900

    選択肢データアップロードのview作成

--- resources/js/components/QuestionUploadComponent.vue
+++ resources/js/components/QuestionUploadComponent.vue
@@ -2,17 +2,28 @@
   <div class="container-fluid">
     <div class="row justify-content-center">
       <div class="col-md-4">
-        <h4 class="border-bottom">過去問データアップロード</h4>
+        <h4 class="border-bottom">問題データアップロード</h4>
         <div class="uploadArea">
           <label class="uploadButton">
-            <input type="file" class="uploadFile" @change="fileSelect" accept=".csv">問題データを選択
+            <input type="file" class="uploadFile" @change="selectQuestion" accept=".csv">問題データを選択
           </label>
         </div>
         <p>{{fileName}}</p>
-        <button type="button" @click="sendFile" class="btn btn-primary">アップロード</button>
+        <button type="button" @click="sendQuestion" class="btn btn-primary">アップロード</button>
       </div>
     </div>
-
+    <div class="row justify-content-center">
+      <div class="col-md-4">
+        <h4 class="border-bottom">選択肢データアップロード</h4>
+        <div class="uploadArea">
+          <label class="uploadButton">
+            <input type="file" class="uploadFile" @change="selectChoice" accept=".csv">選択肢データを選択
+          </label>
+        </div>
+        <p>{{fileName}}</p>
+        <button type="button" @click="sendChoice" class="btn btn-primary">アップロード</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -26,11 +37,11 @@ export default {
   },
   
   methods: {
-    fileSelect(e){
+    selectQuestion(e){
       this.questionFile = e.target.files[0];
       this.fileName = this.questionFile.name;
       },
-    sendFile() {
+    sendQuestion() {
       let formdata = new FormData();
       formdata.append('csv_file', this.questionFile);
       axios.post('/api/questions/upload', formdata)
@@ -39,12 +50,29 @@ export default {
           this.questionFile = null;
           this.fileName = "";
         });
-    }
+    },
+    selectChoice(e){
+      this.questionFile = e.target.files[0];
+      this.fileName = this.questionFile.name;
+    },
+    sendChoice() {
+      let formdata = new FormData();
+      formdata.append('csv_file', this.questionFile);
+      axios.post('/api/questions/upload', formdata)
+        .then((res) => {
+          console.log(res);
+          this.questionFile = null;
+          this.fileName = "";
+        });
+    },
   }
 }
 </script>
 
 <style>
+.row{
+  margin-bottom: 30px;
+}
 .uploadArea{
   margin: 15px 0;
 }
