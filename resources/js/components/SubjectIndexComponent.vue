f35d3cd2304bc218e5ab968dcf723b208ce6b51e
Masa H <masamasa0669@gmail.com>
2021-11-05 13:03:08 +0900

    マテリアルデザイン完成

--- resources/js/components/SubjectIndexComponent.vue
+++ resources/js/components/SubjectIndexComponent.vue
@@ -3,11 +3,9 @@
     <div class="subjectMenu">
       <h2 style="text-align: center">科目を選んでください</h2>
       <div class="subjectMenu-list">
-        <div class="subjectIcon" v-for="(subject, index) in subjects" :key="index" >
-          <router-link class="subjectLink" v-bind:to="{name: 'subject.show', params: {subjectId: subject.id.toString()}}">
-            {{subject.name}}
-          </router-link>
-        </div>
+        <router-link  class="subjectIcon" v-for="(subject, index) in subjects" :key="index" v-bind:to="{name: 'subject.show', params: {subjectId: subject.id.toString()}}">
+          {{subject.name}}
+        </router-link>
       </div>
     </div>
   </div>
@@ -38,30 +36,29 @@ export default {
 
 <style>
 .subjectMenu{
-  max-width: 750px;
+  max-width: 700px;
   margin: 0 auto;
 }
 .subjectMenu-list{
   padding: 20px;
   display: flex;
   justify-content: space-between;
+  flex-wrap: wrap;
+
 }
 
 .subjectIcon{
   width: 150px;
   height: 150px;
+  margin-bottom: 20px;
   background-color: cornflowerblue;
   border-radius: 1rem;
-  display: inline-block;
   text-align: center;
-
-}
-
-.subjectLink{
   font-size: 18px;
   color: white;
   line-height: 150px;
 }
+
 a:hover{
   color: white;
   text-decoration: none;
